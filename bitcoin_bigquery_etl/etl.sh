9ccdd44ee5015c7104ab12cd0303f9a0d756c213
Colin Bookman <cobookman@gmail.com>
2018-02-07 21:41:46 -0600

    dedup before ETL

--- bitcoin_bigquery_etl/etl.sh
+++ bitcoin_bigquery_etl/etl.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 DESTINATION="bigquery-public-data:bitcoin_blockchain.transactions"
 BILLING_PROJECT="bitcoin-bigquery"
-
+# Dedup duplicate blocks
 QUERY=""
+QUERY="WITH dedup AS( "
+QUERY+=" SELECT "
+QUERY+="   * "
+QUERY+=" FROM ( "
+QUERY+="   SELECT "
+QUERY+="     *, "
+QUERY+="     ROW_NUMBER() OVER (PARTITION by block_id) row_number "
+QUERY+="   FROM "
+QUERY+="     \`bitcoin-bigquery.bitcoin.blocks\` "
+QUERY+=" ) "
+QUERY+=" WHERE "
+QUERY+="   row_number = 1) "
+
+# Unpack transactions as unique row
 QUERY+="SELECT "
 QUERY+="  timestamp, "
 QUERY+="  transactions.*, "
 QUERY+="  block_id, "
 QUERY+="  previous_block, "
 QUERY+="  merkle_root, "
-QUERY+="  difficultyTarget, "
 QUERY+="  nonce, "
-QUERY+="  version "
+QUERY+="  version, "
+QUERY+="  work_terahash, "
+QUERY+="  work_error "
 QUERY+="FROM "
-QUERY+="  \`bitcoin-bigquery.bitcoin.blocks_raw\` "
+QUERY+="  dedup "
 QUERY+="JOIN "
 QUERY+="  UNNEST(transactions) AS transactions "
 
